c7173cb92007d8bb874b05545e1041947e3a9c8f
dmh <dmytro.hrynevych@gmail.com>
2016-06-02 19:51:20 +0300

    update

--- main.js
+++ main.js
@@ -187,6 +187,29 @@ jQuery(function($) {
 
 })(jQuery);
 
+(function($) {
+    'use strict';
+
+    // document load event
+    $(document).ready(function() {
+        // Parallax
+        // https://github.com/nk-o/jarallax
+        $('.parallax-img').jarallax({
+            type: 'scroll', //scroll, scale, opacity, scroll-opacity, scale-opacit
+            speed: 0.5,
+            noAndroid: false,
+            noIos: true
+        });
+        $('.parallax-video').jarallax({
+            type: 'scroll', //scroll, scale, opacity, scroll-opacity, scale-opacit
+            speed: 0.5,
+            noAndroid: true,
+            noIos: true
+        });
+    });
+
+})(jQuery);
+
 
 /*global tx_solr_suggestUrl*/
 /*global Awesomplete*/
